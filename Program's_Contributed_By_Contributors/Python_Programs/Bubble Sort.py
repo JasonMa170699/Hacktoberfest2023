ddcbbac9f5d392ff458b4d54555d44af49c68f65
JasonMa170699 <146371283+JasonMa170699@users.noreply.github.com>
2023-10-22 23:58:35 -0700

    Update Bubble Sort.py

--- Program's_Contributed_By_Contributors/Python_Programs/Bubble Sort.py
+++ Program's_Contributed_By_Contributors/Python_Programs/Bubble Sort.py
@@ -1,26 +1,24 @@
 # Python3 program for Bubble Sort Algorithm Implementation
-def bubbleSort(arr):
-	
-	n = len(arr)
+def bubble_sort(arr):
+    # Get the length of the array
+    n = len(arr)
 
-	# For loop to traverse through all 
-	# element in an array
-	for i in range(n):
-		for j in range(0, n - i - 1):
-			
-			# Range of the array is from 0 to n-i-1
-			# Swap the elements if the element found 
-			#is greater than the adjacent element
-			if arr[j] > arr[j + 1]:
-				arr[j], arr[j + 1] = arr[j + 1], arr[j]
-				
+    # Traverse through all elements in the array
+    for i in range(n):
+        # The inner loop iterates through the unsorted part of the array
+        for j in range(0, n - i - 1):
+            # Swap the elements if the current element is greater than the next element
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
 # Driver code
+if __name__ == "__main":
+    # Example to test the above code
+    arr = [2, 1, 10, 23]
 
-# Example to test the above code
-arr = [ 2, 1, 10, 23 ]
+    # Call the bubble_sort function to sort the array
+    bubble_sort(arr)
 
-bubbleSort(arr)
-
-print("Sorted array is:")
-for i in range(len(arr)):
-	print("%d" % arr[i])
+    print("Sorted array is:")
+    for element in arr:
+        print(element)
